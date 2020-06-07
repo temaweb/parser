139c4dd2566bef8e3a358cde487ad7407690c546
temaweb <temaweb@LAPTOP-7AQM00LI.Dlink>
2020-06-07 14:08:02 +0300

    Рефакторинг install.sh и readme

---
+++ README.md
@@ -0,0 +1,100 @@
+# Малахит
+*Сервис парсинга цен*
+
+Демон работает по HTTP, получая ссылку на страницу с ценой и возвращая нормализованую строку с ценой.
+
+# Справка
+
+Запустите `install.sh` с ключем `-h|--help` для получения справки о работе инсталлятора
+
+```sh
+$ ./install.sh --help
+
+NAME
+    install.sh - Сценарий установки сервиса парсинга цен.
+
+SYNOPSIS
+    install.sh [-h|--help]
+    install.sh [-u|--url <arg>]
+               [-s|--service <arg>]
+               [-v|--version <arg>]
+               [-e|--env <arg>]
+               [--uninstall]
+
+OPTIONS
+  -h, --help        
+        Показывает справку
+
+  -u, --url <arg>        
+        Определяет URL сервиса включая протокол, хост и порт.
+        По-умолчанию: http://*:5050
+
+  -s, --service <arg>        
+        Имя сервиса для systemctl
+        По-умолчанию: malachite-parser
+  
+  -e, --env <arg>        
+        Рабочее окружение. Устанавливает значение для ASPNETCORE_ENVIRONMENT
+        Выбор: Development Production Debug
+        По-умолчанию: Production
+
+  -v, --version <arg>        
+        Версия сервиса
+        По-умолчанию: 1.0  
+
+  --uninstall        
+        Удаляет демон и приложение 
+```
+
+# Инсталляция
+
+Для инсталляции клонируйте этот репозиторий и запустите install.sh под суперпользователем
+```sh
+$ git clone http://gitlab.lifeit.com/LifeIT/Malakhit/parser.git
+$ cd parser
+$ sudo ./install.sh
+```
+
+После успешной инсталляции будет создан пользователь malachite:malachite и запущен демон под этим пользователем  
+
+```
+● malachite-parser.service - Malachite link parser service
+   Loaded: loaded (/usr/lib/systemd/user/malachite-parser.service; enabled; vendor preset: disabled)
+   Active: active (running) since Sun 2020-06-07 13:55:43 MSK; 9ms ago
+ Main PID: 128284 (dotnet)
+    Tasks: 1 (limit: 100028)
+   Memory: 756.0K
+   CGroup: /system.slice/malachite-parser.service
+           └─128284 /bin/dotnet /var/www/malachite-parser/Malachite.Parser.WebApi.dll --urls http://*:5050
+```
+
+# Удаление
+
+Для удаление запустите скрипт с ключем `--uninstall`
+
+```sh
+$ sudo ./install.sh --uninstall
+```
+
+### Конфигурирация 
+
+Параметры установки по-умолчанию:
+
+```sh
+URL = http://*:5050
+
+# Имя сервиса для systemd
+SERVICE = malachite-parser
+
+# Директория для исполняемых файлов
+SERVICEDIR = /var/www/malachite-parser
+
+# Путь к конфигураицонному файлу демона
+SERVICECONFIGPATH = /usr/lib/systemd/user/malachite-parser.service
+
+# Версия приложения
+VERSION = 1.0
+
+# Рабочее окружение приложения
+ENVIRONMENT = Production
+```
--- install.sh
+++ install.sh
@@ -1,151 +1,413 @@
 #!/bin/bash
 
-# Host
-URL='http://localhost:5050'
+#
+# Значения по-умолчанию
+#
 
-# Build Proj
-PROJECT='Malachite.Parser.WebApi'
+ENVIRONMENTS=(Development Production Debug)
 
-# Build configuration
-CONFIGURATION='Release' # Debug | Release
+DEFAULT_URL='http://*:5050'
+DEFAULT_SERVICE='malachite-parser'
+DEFAULT_VERSION='1.0'
+DEFAULT_ENVIRONMENT=${ENVIRONMENTS[1]}
 
-# Application version
-VERSION='1.0'
+#
+# Конфигурируемые параметры из аргументов командной строки
+#
+
+URL=$DEFAULT_URL
+VERSION=$DEFAULT_VERSION
+SERVICE=$DEFAULT_SERVICE
+
+
+#
+# .NET and Project settings
+#
 
 # ASP NET Core App version requred
 ASPNETCOREAPP="Microsoft.AspNetCore.App 3.1"
 
-# Environment
-ASPNETCORE_ENVIRONMENT='Production' 
+# Path to .NET
+DOTNETPATH=''
 
-# Service name
-SERVICE='malachite-parser'
-
-# Current directory
-CURRENT=$PWD # "$(dirname "$0")"
-
-# Path to service directory 
-SERVICEDIR="/var/www/$SERVICE"
+# Build Proj
+PROJECT='Malachite.Parser.WebApi'
 
 # Project directory
-PROJDIR="$CURRENT/$PROJECT"
+PROJDIR="$PWD/$PROJECT"
+
+# Build configuration
+CONFIGURATION='Release' # Debug | Release
+
+
+#
+# Параметры сервиса
+#
+
+# Path to service directory 
+SERVICEDIR="/var/www/${SERVICE}"
 
 # Service config path
-SERVICECONFIGPATH=/usr/lib/systemd/user/$SERVICE.service
+SERVICECONFIGPATH="/usr/lib/systemd/user/${SERVICE}.service"
 
 # Path to service library
 SERVICEDLLPATH=$SERVICEDIR/$PROJECT.dll
 
-# User
+# $SERVICEUSER home directory
+SERVICEUSERHOME=''
+
+# Пользователь под которым будет запущен сервис
 SERVICEUSER='malachite'
 
-# Group
+# Группа под которой будет запущен сервис
 SERVICEGROUP='malachite'
 
-if [ "$(whoami)" != 'root' ]; 
-then
-    echo "You have no permission to run $0 as non-root user."
-    exit 1;
-fi
 
-if ! [ -x "$(command -v dotnet)" ]; 
-then
-    echo 'Error: dotnet is not installed.' >&2
+#
+# Окружение
+#
+
+ASPNETCORE_ENVIRONMENT=$DEFAULT_ENVIRONMENT
+DOTNET_PRINT_TELEMETRY_MESSAGE='True'
+
+# Признак режима удаления
+ISUNINSTALL=false
+
+
+#
+# Справка
+#
+
+usage() 
+{
+  cat - >&2 <<EOF
+NAME
+    install.sh - Сценарий установки сервиса парсинга цен.
+
+SYNOPSIS
+    install.sh [-h|--help]
+    install.sh [-u|--url <arg>]
+               [-s|--service <arg>]
+               [-v|--version <arg>]
+               [-e|--env <arg>]
+               [--uninstall]
+
+OPTIONS
+  -h, --help        
+        Показывает справку
+
+  -u, --url <arg>        
+        Определяет URL сервиса включая протокол, хост и порт.
+        По-умолчанию: ${DEFAULT_URL}
+
+  -s, --service <arg>        
+        Имя сервиса для systemctl
+        По-умолчанию: ${DEFAULT_SERVICE}
+  
+  -e, --env <arg>        
+        Рабочее окружение. Устанавливает значение для ASPNETCORE_ENVIRONMENT
+        Выбор: ${ENVIRONMENTS[*]}
+        По-умолчанию: ${DEFAULT_ENVIRONMENT}
+
+  -v, --version <arg>        
+        Версия сервиса
+        По-умолчанию: ${DEFAULT_VERSION}  
+
+  --uninstall        
+        Удаляет демон и приложение 
+
+EOF
+}
+
+
+#
+# Показывает опции инсталляции 
+#
+
+print_install_options()
+{
+    cat - >&2 <<EOF2
+
+    Установка сервиса парсинга цен
+
+    URL = ${URL}
+    
+    SERVICE           = ${SERVICE}
+    SERVICEDIR        = ${SERVICEDIR}
+    SERVICECONFIGPATH = ${SERVICECONFIGPATH}
+
+    VERSION           = ${VERSION}
+    ENVIRONMENT       = ${ASPNETCORE_ENVIRONMENT}
+
+EOF2
+}
+
+
+#
+# Авариайное завершение с сообщением
+#
+
+fatal() 
+{
+    for i; do
+        echo -e "\033[0;31mFatal: ${i}\033[0m" >&2
+    done
     exit 1
-fi
+}
 
-if ! [ "$(dotnet --list-runtimes | grep "$ASPNETCOREAPP")" ];
-then
-    echo "Error: $ASPNETCOREAPP is not installed." >&2
-    exit 1
-fi
+#
+# Штатное завершение с сообщением
+#
 
-if systemctl is-active --quiet $SERVICE ;
-then
-    echo "Detect $SERVICE is running. Stopping service..." >&2
+success()
+{
+    for i; do
+        echo -e "\033[32m${i}\033[0m" >&2
+    done
+    exit 0
+}
 
-    # Stop $SERVICE 
+#
+# Разбор аргументов командной строки
+#
+
+parse_command_line_arg()
+{    
+    while [[ $# -gt 0 ]]
+    do
+        key="$1"  
+
+        case $key in
+            -u|--url)
+                URL="$2"
+                shift
+                shift
+            ;;
+            -s|--service)
+                SERVICE="$2"
+                shift
+                shift
+            ;;
+            -v|--version)
+                VERSION="$2"
+                shift
+                shift 
+            ;;
+            -e|--env)
+
+                if [[ ! " ${ENVIRONMENTS[@]} " =~ " $2 " ]]; 
+                then
+                    fatal "Неправильный аргумент --env ${2}, допустимые параметры: ${ENVIRONMENTS[*]}"
+                fi
+
+                ASPNETCORE_ENVIRONMENT="$2"
+                shift
+                shift 
+            ;;
+            --uninstall)
+                ISUNINSTALL=true
+                shift
+            ;;
+            -h|--help|*)
+                usage
+                return 1;
+            ;;
+        esac
+    done
+
+    return 0;
+}
+
+
+#
+# Останавливает сервис $SERVICE 
+#
+
+stop_service() 
+{
     systemctl stop $SERVICE 
 
     if [ $? -eq 1 ]; 
     then
-        echo "Service $SERVICE stopped failed" >&2
-        exit 1
+        fatal "Service $SERVICE stopped failed"
     else
-        echo "Service $SERVICE stopped" >&2  
+        echo -e "\033[32mService $SERVICE stopped\033[0m" >&2  
     fi
-fi
+}
 
-# Create user group if not exist
-getent group $SERVICEGROUP || groupadd $SERVICEGROUP
 
-# Create user if not exist
-if ! [[ -n $(id -u $SERVICEUSER 2>/dev/null) ]];
-then
-    echo "User $SERVICEUSER does not exist" >&2
-    echo "Create user $SERVICEUSER:$SERVICEGROUP" >&2
+#
+# Проверяет установку .NET Core
+# 
 
-    # Create user
-    useradd -g $SERVICEGROUP $SERVICEUSER 
+dotnet_checks()
+{
+    if ! [ -x "$(command -v dotnet)" ]; 
+    then
+        fatal 'dotnet is not installed.'
+    fi
 
-    # Add user to group $SERVICEGROUP
-    # usermod -a -G $SERVICEGROUP $SERVICEUSER
-fi
+    if ! [ "$(dotnet --list-runtimes | grep "$ASPNETCOREAPP")" ];
+    then
+        fatal "${ASPNETCOREAPP} is not installed."
+    fi
 
-# Remove service directory if exist
-rm -rf $SERVICEDIR
+    DOTNETPATH=$(which dotnet)
+}
 
-# Build and publish binaries
-dotnet publish "$PROJDIR/$PROJECT.csproj" \
-    -c $CONFIGURATION \
-    -o $SERVICEDIR \
-    --version-suffix $VERSION \
-    --no-self-contained \
-    --nologo
 
-# Change owner to $SERVICEUSER
-chown -R $SERVICEUSER:$SERVICEGROUP $SERVICEDIR/*
+#
+# Создает пользователя и группу для сервиса
+#
 
-# $SERVICEUSER home directory
-SERVICEUSERHOME=$(eval echo ~$SERVICEUSER)
+create_service_user()
+{
+    # Create user group if not exist
+    getent group $SERVICEGROUP || groupadd $SERVICEGROUP
 
-# Path to dotnet
-DOTNETPATH=$(which dotnet)
+    # Create user if not exist
+    if ! [[ -n $(id -u $SERVICEUSER 2>/dev/null) ]];
+    then
+        echo "User $SERVICEUSER does not exist" >&2
+        echo "Create user $SERVICEUSER:$SERVICEGROUP" >&2
 
-# Create service config
-touch $SERVICECONFIGPATH
+        # Create user
+        useradd -g $SERVICEGROUP $SERVICEUSER 
 
-# Set permissions (-rw-rw-r--)
-chmod 664 $SERVICECONFIGPATH
+        # Add user to group $SERVICEGROUP
+        # usermod -a -G $SERVICEGROUP $SERVICEUSER
+    fi
 
-# Write file content
-cat > $SERVICECONFIGPATH <<EOF
+    SERVICEUSERHOME=$(eval echo ~$SERVICEUSER)
+}
+
+
+#
+# Билд и публикация сервиса в $SERVICEDIR
+#
+
+install_service() 
+{
+    # Remove service directory if exist
+    rm -rf $SERVICEDIR
+
+    # Build and publish binaries
+    dotnet publish "$PROJDIR/$PROJECT.csproj" \
+        -c $CONFIGURATION \
+        -o $SERVICEDIR \
+        --version-suffix $VERSION \
+        --no-self-contained \
+        --nologo
+
+    # Change owner to $SERVICEUSER
+    chown -R $SERVICEUSER:$SERVICEGROUP $SERVICEDIR/*
+}
+
+
+#
+# Uninstall
+#
+
+uninstall()
+{
+    stop_service
+
+    # Enabled
+    systemctl disable $SERVICE
+
+    # Reload
+    systemctl daemon-reload
+
+    # Remove service directory and service config
+    rm -rf $SERVICEDIR
+    rm -f $SERVICECONFIGPATH
+
+    return 0;
+}
+
+
+#
+# Создает конфиграцию сервиса в $SERVICECONFIGPATH
+#
+
+create_service_config()
+{
+    # Create service config
+    touch $SERVICECONFIGPATH
+
+    # Set permissions (-rw-rw-r--)
+    chmod 664 $SERVICECONFIGPATH
+
+    # Write file content
+    cat > $SERVICECONFIGPATH <<EOF
 [Unit]
 Description=Malachite link parser service
 
 [Service]
 Type=simple
-User=$SERVICEUSE
-Group=$SERVICEGROUP
-WorkingDirectory=$SERVICEDIR
-ExecStart=$DOTNETPATH $SERVICEDLLPATH --urls $URL
+User=${SERVICEUSER}
+Group=${SERVICEGROUP}
+WorkingDirectory=${SERVICEDIR}
+ExecStart=${DOTNETPATH} ${SERVICEDLLPATH} --urls ${URL}
 StandardOutput=journal
 KillMode=process
-SyslogIdentifier=$SERVICE
-Environment=ASPNETCORE_ENVIRONMENT=$ASPNETCORE_ENVIRONMENT
-Environment=DOTNET_PRINT_TELEMETRY_MESSAGE=true
-Environment=DOTNET_CLI_HOME=$SERVICEUSERHOME
+SyslogIdentifier=${SERVICE}
+Environment=ASPNETCORE_ENVIRONMENT=${ASPNETCORE_ENVIRONMENT}
+Environment=DOTNET_PRINT_TELEMETRY_MESSAGE=${DOTNET_PRINT_TELEMETRY_MESSAGE}
+Environment=DOTNET_CLI_HOME=${SERVICEUSERHOME}
 
 [Install]
 WantedBy=multi-user.target
 EOF
+}
 
-# Enbled
-systemctl enable $SERVICECONFIGPATH
 
-# Reload
-systemctl daemon-reload
+#
+# Запускает сервис
+#
 
-# Start service
-systemctl start  $SERVICE
-systemctl status $SERVICE
+run_service()
+{
+    # Enabled
+    systemctl enable $SERVICECONFIGPATH
+
+    # Reload
+    systemctl daemon-reload
+
+    # Start service
+    systemctl start  $SERVICE
+    systemctl status $SERVICE
+}
+
+
+# root
+
+{
+    parse_command_line_arg "$@" || exit 0
+
+    if [ "$ISUNINSTALL" = true ];
+    then
+        uninstall && success 'Приложение успешно удалено'
+    fi
+
+    print_install_options
+
+    if [ "$(whoami)" != 'root' ]; 
+    then
+        fatal "You have no permission to run $0 as non-root user."
+    fi
+
+    dotnet_checks
+
+    if systemctl is-active --quiet $SERVICE ;
+    then
+        echo "Detect $SERVICE is running. Stopping service..." >&2
+        stop_service
+    fi
+
+    create_service_user
+    create_service_config
+
+    install_service  
+    run_service  
+}
